fe06d44cb03b1f478a75c4fb6a2ff0e5362dc4fa
yamolean <moririnmanro9608@gmail.com>
2020-03-01 20:18:53 +0900

    first commit

---
+++ EmolsComponent/EmolsComponent.h
@@ -0,0 +1,19 @@
+//
+//  EmolsComponent.h
+//  EmolsComponent
+//
+//  Created by 矢守叡 on 2020/03/01.
+//  Copyright © 2020 yamolean. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EmolsComponent.
+FOUNDATION_EXPORT double EmolsComponentVersionNumber;
+
+//! Project version string for EmolsComponent.
+FOUNDATION_EXPORT const unsigned char EmolsComponentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmolsComponent/PublicHeader.h>
+
+
