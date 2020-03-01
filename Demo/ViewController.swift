b1626aaf01aea2da165addb9b338ab56adac19d1
yamolean <moririnmanro9608@gmail.com>
2020-03-01 20:24:30 +0900

    init Demo project

---
+++ Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by 矢守叡 on 2020/03/01.
+//  Copyright © 2020 yamolean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
